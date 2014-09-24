28a7c007a514001246fe8e4c32cbb726e13a13a1
Jeremy H <secretmojo@g-m-a-i-l.com>
2014-09-24 18:20:56 -0500

    Filter out games of 0 second duration, cuz who wants to watch THAT?

--- ui/mods/SentientReplays/SentientReplays_replay_browser.js
+++ ui/mods/SentientReplays/SentientReplays_replay_browser.js
@@ -5,6 +5,19 @@
 // interesting to watch :-)
 
 
+/// --- debug function ---
+model.showMembers = function (obj) {
+  var result = [];
+  for (var id in obj) {
+    try {
+        console.log(id + ": " + obj[id].toString());
+    } catch (err) {
+      console.log(id + ": inaccessible");
+    }
+  }
+  return result;
+}
+
 // ----------------- HTML Hackery -----------------------
 // inject two checkboxes in the search div, 
 // one to "Show Galactic Wars" and the other to "Show AI Games".
@@ -45,6 +58,7 @@ model.filteredGameList = ko.computed({read: function () {
             var allGames = model.gameList().concat(model.lanGameList());
             var filteredGames = [];
             var selectedGameStillVisible = false;
+            //model.showMembers (allGames[0]);
 
             _.forEach(allGames, function(game){
 
@@ -54,10 +68,16 @@ model.filteredGameList = ko.computed({read: function () {
 
                 if (!matched) return;
                 
+
+                
                 // CHANGED CODE START
                   // These actually should access proper fields in the game
                   // record, but I have a total lack of knowledge how to do that.
                   // So, just filter based on text description.
+                  
+                  // times of 0 seconds can't be watched anyway
+                  if (game.duration.indexOf("0:00") === 0) return;
+                  
                   if (!model.ShowGalacticWars()) {
                     if (game.searchable.indexOf("Galactic War".toUpperCase()) != -1) return;
                   }
@@ -79,9 +99,14 @@ model.filteredGameList = ko.computed({read: function () {
 
                 // If our filters haven't whacked the game from the list, include in our results
                 filteredGames.push(game);
+                
+                ///for (var army in game.armies) {
+                ///      model.showMembers(army);
+                ///}
             });
 
             if (!selectedGameStillVisible) model.setSelected(null);
+
             return filteredGames;
 
         }, deferEvaluation: true});
